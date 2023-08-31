cc4de9279eaf8096705cbbb3b51bb3b77973d343
João Guilherme Domingues <142355184+kazidv@users.noreply.github.com>
2023-08-31 16:49:50 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,24 @@
+1
+console.log(b) //aparecerá no console o numero 10.
+
+2
+
+b = 5
+console.log(a, b) //aparecerá o numero 5.
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) // aparecerá o numero 10,20 e a letra a.
+
+3
+
+let cargaHorariaDiaria = prompt("Quantas horas você trabalha por dia?")
+let ganhoDiario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${cargaHorariaDiaria/ganhoDiario} por hora`)
+
+4
+
