dc652bce35985125f141daf3c63a575ad2efabfc
João Guilherme Domingues <142355184+kazidv@users.noreply.github.com>
2023-09-06 13:59:01 -0300

    Add files via upload

---
+++ desafio-variaveis/index.js
@@ -0,0 +1,9 @@
+const valorA = prompt("Digite um valor:");
+const valorB = prompt("Digite outro valor:");
+const soma = (valorA + valorB)
+console.log(soma)
+
+const ValorC = prompt("Dgite um valor:")
+const ValorD = prompt("Digite outro numero")
+const multiplicação = (ValorC * ValorD)
+console.log(multiplicação)
